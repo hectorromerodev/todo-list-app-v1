aac77d5cbb5d89cca663bccd54ce62d2681422b5
hectorromerodev <hectorromero.developer@gmail.com>
2020-07-02 08:32:37 -0700

    ADD Task service added

---
+++ src/app/services/task.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@angular/core';
+// We need to import HttpClient and Task interface
+import { HttpClient } from '@angular/common/http';
+import { Task } from '../interfaces/task';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TaskService {
+  readonly api = 'https://jsonplaceholder.typicode.com';
+  readonly endpoint = 'todos';
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getAll() {
+    const path = `${this.api}/${this.endpoint}`;
+    return this.http.get<Task[]>(path);
+  }
+
+  getOne(id: string | number) {
+    const path = `${this.api}/${this.endpoint}/${id}`;
+    return this.http.get<Task>(path);
+  }
+
+  create(body: Task) {
+    const path = `${this.api}/${this.endpoint}`;
+    return this.http.post(path, body);
+  }
+
+  update(body: Task, id: string | number) {
+    const path = `${this.api}/${this.endpoint}/${id}`;
+    return this.http.put(path, body);
+  }
+
+  delete(id: string | number) {
+
+  }
+}
