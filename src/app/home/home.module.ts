68b8446948cc715a53aab3f649416e3c8df89514
hectorromerodev <hectorromero.developer@gmail.com>
2020-07-02 08:31:27 -0700

    UPD TaskPage import added

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -5,6 +5,8 @@ import { FormsModule } from '@angular/forms';
 import { HomePage } from './home.page';
 
 import { HomePageRoutingModule } from './home-routing.module';
+import { TaskPage } from '../task/task.page';
+import { TaskPageModule } from '../task/task.module';
 
 
 @NgModule({
@@ -12,8 +14,9 @@ import { HomePageRoutingModule } from './home-routing.module';
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
+    TaskPageModule
   ],
   declarations: [HomePage]
 })
-export class HomePageModule {}
+export class HomePageModule { }
