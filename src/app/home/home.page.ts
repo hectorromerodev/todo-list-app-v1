b5444168910d98023aba4c1d9bae0029be73f4dd
hectorromerodev <hectorromero.developer@gmail.com>
2020-07-02 08:32:18 -0700

    ADD Task UI added

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from '../interfaces/task';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,21 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  importantTask: Task[] = [
+    { title: 'Love my girl', priority: 'important', progress: 0.99 },
+    { title: 'Develop a todo app', priority: 'important', progress: 0.25 },
+    { title: 'Read a book', priority: 'least', progress: 0.89 },
+  ];
+  generalTask: Task[] = [
+    { title: 'Wash the dishes', priority: 'general', progress: 0.20 },
+  ];
+  leastTask: Task[] = [
+    { title: 'Eat an apple', priority: 'least', progress: 0.5 },
+    { title: 'Learn Arduino', priority: 'least', progress: 0.0 },
+  ];
 
-  constructor() {}
+  priority = ['Important', 'General', 'Least'];
+  title = ['Amar a Michelle', 'Work with TODO app', 'Do homework'];
+  constructor() { }
 
 }
