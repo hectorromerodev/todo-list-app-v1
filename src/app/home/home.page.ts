4503d1bd25432f6cd0337b01fa3511240de4217f
hectorromerodev <hectorromero.developer@gmail.com>
2020-07-02 15:54:23 -0700

    UPD home page updated to recive data

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Task } from '../interfaces/task';
+import { TaskService } from '../services/task.service';
+import { AlertController, ModalController } from '@ionic/angular';
+import { TaskModalPage } from '../task/task-modal/task-modal.page';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
   importantTask: Task[] = [
     { title: 'Love my girl', priority: 'important', progress: 0.99 },
     { title: 'Develop a todo app', priority: 'important', progress: 0.25 },
@@ -22,6 +25,52 @@ export class HomePage {
 
   priority = ['Important', 'General', 'Least'];
   title = ['Amar a Michelle', 'Work with TODO app', 'Do homework'];
-  constructor() { }
+  constructor(
+    private taskService: TaskService,
+    private alertCtrl: AlertController,
+    private modalCtrl: ModalController,
 
+  ) { }
+
+  ngOnInit() {
+    this.get();
+  }
+  async get() {
+    await this.taskService.getAll()
+      .subscribe(all => {
+        this.importantTask = all.slice(0, 10);
+      });
+  }
+
+  async deleteAlert() {
+    const alert = await this.alertCtrl.create({
+      header: 'Delete this task?',
+      message: 'Are you sure to do this?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel',
+          handler: () => {
+            console.log('confirm Cancel');
+          }
+        },
+        {
+          text: 'Delete',
+          handler: () => {
+            console.log('Confirm Ok');
+          }
+        }
+      ]
+    });
+    await alert.present();
+  }
+  async openModal() {
+    const modal = await this.modalCtrl.create({
+      component: TaskModalPage,
+      keyboardClose: true,
+
+    });
+
+    return await modal.present();
+  }
 }
