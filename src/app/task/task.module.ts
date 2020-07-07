ae4c2cc29309e89fc081d4b5b1f5a27705dffb8e
hectorromerodev <hectorromero.developer@gmail.com>
2020-07-07 08:36:12 -0700

    ADD NgCircleProgressModule added

--- src/app/task/task.module.ts
+++ src/app/task/task.module.ts
@@ -8,12 +8,35 @@ import { TaskPageRoutingModule } from './task-routing.module';
 
 import { TaskPage } from './task.page';
 
+
+// Import ng-circle-progress
+import { NgCircleProgressModule } from 'ng-circle-progress';
+
+
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    TaskPageRoutingModule
+    TaskPageRoutingModule,
+    // Specify ng-circle-progress as an import
+    NgCircleProgressModule.forRoot({
+      radius: 60,
+      space: -10,
+      maxPercent: 100,
+      outerStrokeGradient: true,
+      outerStrokeWidth: 10,
+      outerStrokeColor: '#4882c2',
+      outerStrokeGradientStopColor: '#53a9ff',
+      innerStrokeColor: '#e7e8ea',
+      innerStrokeWidth: 10,
+      title: 'UI',
+      animateTitle: false,
+      animationDuration: 4000,
+      showUnits: false,
+      showBackground: false,
+      clockwise: false
+    })
   ],
   exports: [
     TaskPage // Export task page to use <app-task> in home
